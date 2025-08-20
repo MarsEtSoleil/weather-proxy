bc8c1abb72a8c0083097f8ffed65f4fbc75a388e
mars_et_soleil <nars_et_soleil@yahoo.co.jp>
2025-08-20 09:33:14 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+﻿const express = require("express");
+const axios = require("axios");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// 天気予報XMLのURL（京都府例）
+const WEATHER_URL = "https://www.drk7.jp/weather/xml/26.xml";
+
+// ルート
+app.get("/", (req, res) => {
+  res.send("Weather XML Proxy API is running! Try /weather");
+});
+
+// プロキシエンドポイント
+app.get("/weather", async (req, res) => {
+  try {
+    const response = await axios.get(WEATHER_URL, {
+      responseType: "text", // XML をそのまま文字列として取得
+    });
+
+    res.set("Content-Type", "application/xml; charset=UTF-8");
+    res.send(response.data);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Error fetching weather XML");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
